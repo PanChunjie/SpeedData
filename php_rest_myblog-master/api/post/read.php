ee44dab9269342c6d6ecefcc8458af255a1071e5
Chunjie Pan <pance@mail.uc.edu>
2019-08-06 18:57:32 -0400

    First

---
+++ php_rest_myblog-master/api/post/read.php
@@ -0,0 +1,67 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  echo json_encode(
+    array('message' => 'Posts Found')
+  );
+ // include_once '../../config/Database.php';
+ // include_once '../../models/Post.php';
+  
+  //  $html = "";
+  //  $xmlstr = simplexml_load_file("http://api.openweathermap.org/data/2.5/weather?zip=45220,us&APPID=8ea80fceb1c77f7b6e101fd61bf5076c&mode=xml");
+
+  //  $xmlobj = new SimpleXMLElement($xmlstr);
+  //  $xmlobj = (array)$xmlobj;//optional
+    //$xml = simplexml_load_file($url);
+   // print_r($xmlstr);
+  
+  // print_r('asda');
+  
+ // echo $url;
+  // Instantiate DB & connect
+ // $database = new Database();
+ // $db = $database->connect();
+
+  // Instantiate blog post object
+ // $post = new Post($db);
+
+  // Blog post query
+ // $result = $post->read();
+  // Get row count
+  // $num = 5;
+
+  // // Check if any posts
+  // if($num > 0) {
+  //   // Post array
+  //   $posts_arr = array();
+  //   // $posts_arr['data'] = array();
+
+  //   // while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  //   //   extract($row);
+
+  //   //   $post_item = array(
+  //   //     'id' => $id,
+  //   //     'title' => $title,
+  //   //     'body' => html_entity_decode($body),
+  //   //     'author' => $author,
+  //   //     'category_id' => $category_id,
+  //   //     'category_name' => $category_name
+  //   //   );
+
+  //   //   // Push to "data"
+  //   //   array_push($posts_arr, $post_item);
+  //   //   // array_push($posts_arr['data'], $post_item);
+  //   // }
+
+  //   // Turn to JSON & output
+  //  echo json_encode(
+  //    array('message' => 'Posts Found')
+  //  );
+
+  // } else {
+  //   // No Posts
+  //  echo json_encode(
+  //    array('message' => 'Posts Not Found')
+  //   );
+  // }
---
+++ php_rest_myblog-master/api/post/update.php
@@ -0,0 +1,46 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+ // header('Content-Type: application/json');
+ // header('Access-Control-Allow-Methods: PUT');
+ // header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+ // include_once '../../config/Database.php';
+ // include_once '../../models/Post.php';
+
+  $data = trim(file_get_contents('php://input'));
+  $dataPOST = "This ";
+  file_put_contents("myxmlfile.xml", $data);
+  $xmlData = simplexml_load_string($data);    
+  
+  print_r($xmlData);
+  ?>
+  <!-- // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // Set ID to update
+  $post->id = $data->id;
+
+  $post->title = $data->title;
+  $post->body = $data->body;
+  $post->author = $data->author;
+  $post->category_id = $data->category_id;
+
+  // Update post
+  if($post->update()) {
+    echo json_encode(
+      array('message' => 'Post Updated')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Post Not Updated')
+    );
+  } -->
+
