32ba994e56351667f653aae15b78351d3157e6c1
Chunjie Pan <pance@mail.uc.edu>
2019-08-06 21:32:15 -0400

    database paras

--- php_rest_myblog-master/config/Database.php
+++ php_rest_myblog-master/config/Database.php
@@ -1,22 +1,28 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'myblog';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'ec2-174-129-226-232.compute-1.amazonaws.com';
+    private $db_name = 'd7ken1r1teta2e';
+    private $username = 'smjvlbeupqpqtc';
+    private $password = '111f398800f92d0708dabfe7603f8c44da14311fb6eb861e1a0b41f6febdc2a2';
+    private $port = "5432";
     private $conn;
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
-
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
+      // $this->conn = null;
+      if($this->conn == null || $this->conn->connection_status() != 0){
+        print_r("connect database");
+        try { 
+          $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+        }
+      }else{
+        print_r("connect database");
       }
+      
 
       return $this->conn;
     }
