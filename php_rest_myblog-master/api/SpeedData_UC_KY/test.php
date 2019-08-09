0e19ab95a5e4e1f81eaf6dba11d11385aa4393af
Chunjie Pan <pance@mail.uc.edu>
2019-08-09 11:15:39 -0400

    delete session

--- php_rest_myblog-master/api/SpeedData_UC_KY/test.php
+++ php_rest_myblog-master/api/SpeedData_UC_KY/test.php
@@ -1,17 +1,17 @@
 <?php
 
-session_start();
-echo session_id();
+// session_start();
+// echo session_id();
 
-if (!isset($_COOKIE['token'])) {
-    setcookie('token', session_id());
-    echo "isset";
-}else{
-    if($_COOKIE['token'] == session_id()){
-        echo "equal = " . $_COOKIE['token'];
-    }else{
-        setcookie('token', session_id());
-        echo "not equal = " . $_COOKIE['token'];
-    }
-}
+// if (!isset($_COOKIE['token'])) {
+//     setcookie('token', session_id());
+//     echo "isset";
+// }else{
+//     if($_COOKIE['token'] == session_id()){
+//         echo "equal = " . $_COOKIE['token'];
+//     }else{
+//         setcookie('token', session_id());
+//         echo "not equal = " . $_COOKIE['token'];
+//     }
+// }
 
