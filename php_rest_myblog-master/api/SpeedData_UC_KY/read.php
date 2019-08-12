be1ed12e36c49cd398007a18f0c4768327628da5
Chunjie Pan <pance@mail.uc.edu>
2019-08-12 11:54:19 -0400

    read

--- php_rest_myblog-master/api/SpeedData_UC_KY/read.php
+++ php_rest_myblog-master/api/SpeedData_UC_KY/read.php
@@ -14,33 +14,34 @@ $database = new Database();
 $db = $database->connect();
 // Instantiate user object
 $user = new UserInfo($db);
+echo "before user";
 //echo $data;
 $user->username = $_GET['username'];
 $user->userpassword = $_GET['password'];
-
-if ($user->checkPassword()) {
-    if ($user->isposter || $user->isadmin || $user->isreader) {
+echo "before check";
+// if ($user->checkPassword()) {
+//     if ($user->isposter || $user->isadmin || $user->isreader) {
         
-        // Instantiate blog post object
-        $post = new SpeedData_UC_KY($db);
+//         // Instantiate blog post object
+//         $post = new SpeedData_UC_KY($db);
 
-        //post query
-        $result = $post->read();
+//         //post query
+//         $result = $post->read();
 
-        // Get row count
-        $num = $result->rowCount();
+//         // Get row count
+//         $num = $result->rowCount();
 
-        // // Check if any posts
-        if ($num > 0) {
-            $row = $result->fetch(PDO::FETCH_ASSOC);
-            echo htmlspecialchars_decode($row[body]);
+//         // // Check if any posts
+//         if ($num > 0) {
+//             $row = $result->fetch(PDO::FETCH_ASSOC);
+//             echo htmlspecialchars_decode($row[body]);
 
-        } else {
-            //   // No Posts
-            echo "Data not found;";
-        }
-    } else {
-      echo "You don't have permission to read data;";
-    }
+//         } else {
+//             //   // No Posts
+//             echo "Data not found;";
+//         }
+//     } else {
+//       echo "You don't have permission to read data;";
+//     }
 
-}
+// }
